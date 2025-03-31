86df2dbfae3ec31ef3583cafea9783a4cd187133
EduardoDavilaDrRaccon <eduardodavila94@hotmail.com>
2025-03-31 11:56:17 -0600

    Adición de nodos de la sesión w1s2

---
+++ ros2_ws/src/my_py_package/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'my_py_package'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='eduardo',
+    maintainer_email='eduardo@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            "node_publisher_py = my_py_package.publisher_py:main",
+            "node_subscriber_py = my_py_package.subscriber_py:main"
+        ],
+    },
+)
