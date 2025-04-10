4ad30f10aa5b5f8b47c0b2f9f52763f3b875368c
EduardoDavilaDrRaccon <eduardodavila94@hotmail.com>
2025-04-10 11:18:42 -0600

    Paquetes de w2s4 y w3s5

---
+++ ros2_ws/src/s4_py_apps/s4_py_apps/subscriber.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from s4_custom_interface.msg import HardwareStatus
+from s4_custom_interface.msg import Sphere
+
+class PySubscriber(Node):
+    def __init__(self):
+        super().__init__('py_subscriber_node')
+        self.get_logger().info("Python Subscriber node has been started")
+        self.subscription = self.create_subscription(
+            HardwareStatus, 'cpp_topic', self.listener_callback, 10)
+        self.subscription2 = self.create_subscription(
+            Sphere, 'py_sphere_topic', self.sphere_callback, 10)
+        self.subscription  # prevent unused variable warning
+        self.subscription2 # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info("Publishing Status: Temperature: %d, Motors Up: %s, Debug Message: %s, Time: %d.%09d" % (
+                    msg.temperature,
+                    msg.are_motors_up,
+                    msg.debug_message,
+                    msg.the_time.sec,
+                    msg.the_time.nanosec))
+    
+    def sphere_callback(self, msg):
+        self.get_logger().info("Publishing Sphere: x: %f, y: %f, z: %f, Radius: %f" % (
+                    msg.center.x,
+                    msg.center.y,
+                    msg.center.z,
+                    msg.radius))
+
+def main(args=None):
+    rclpy.init(args=args)
+    py_subscriber = PySubscriber()
+    try:
+        rclpy.spin(py_subscriber)
+    except KeyboardInterrupt:
+        py_subscriber.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
