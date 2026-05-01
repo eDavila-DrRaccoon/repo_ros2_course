19005929fc89a36dc28b64dc936a7dd5ec1cd5c0
EduardoDavilaDrRaccoon <eduardodavila94@hotmail.com>
2026-05-01 15:28:39 -0600

    Adding original openslam pkgs in the repo root

---
+++ openslam_pkgs/slam_gmapping/launch/slam_gmapping.launch.py
@@ -0,0 +1,14 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    
+    return LaunchDescription([
+        Node(
+            package='slam_gmapping', 
+            executable='slam_gmapping', 
+            output='screen',
+            parameters=[os.path.join(get_package_share_directory("slam_gmapping"), "params", "slam_gmapping.yaml")]),
+    ])
